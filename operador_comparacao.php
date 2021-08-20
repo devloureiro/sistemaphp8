60c2d22eb1603ab7492c4ef906e0dea31cbe1e20
Osmar Loureiro <not@google.com.br>
2021-08-20 14:33:41 +0900

    mandando os operadores

---
+++ operador_comparacao.php
@@ -0,0 +1,10 @@
+<?php 
+
+$valorA = "10";
+$valorB = 10;
+var_dump($valorA === $valorB);
+var_dump(10 > 2);
+
+// para aparecer valores que nao forem nulos
+$valorC = NULL;
+var_dump($valorC ?? "o valor esta nulo");
